854adb2dad55a0b2c399a311253a02e6998bead0
TJ DeMay <57812487+tomdemay@users.noreply.github.com>
2024-06-22 20:28:59 +0000

    formatting and some minor content tweaks

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
-#jekyll server -o -l
- jekyll serve --watch --livereload --force_polling
+#!/bin/bash
+git config --global user.email "57812487+tomdemay@users.noreply.github.com"
+git config --global user.name "TJ DeMay"
+jekyll serve --watch --livereload --force_polling
